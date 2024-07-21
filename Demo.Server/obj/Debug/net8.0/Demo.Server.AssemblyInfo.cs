3968400c712673ce6fc8a825166f91fb8a0b4e01
DerPizzaBursche <nick.roelfs@t-online.de>
2024-07-21 11:16:51 +0200

    Added Libary

--- Demo.Server/obj/Debug/net8.0/Demo.Server.AssemblyInfo.cs
+++ Demo.Server/obj/Debug/net8.0/Demo.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15315611e405761c9032e832676949afc4911a09")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
