15315611e405761c9032e832676949afc4911a09
DerPizzaBursche <nick.roelfs@t-online.de>
2024-07-21 11:12:52 +0200

    init

---
+++ demo.client/next.config.mjs
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import child_process from 'child_process';
+import { env } from 'process';
+import { fileURLToPath } from 'url';
+
+const baseFolder =
+  env.APPDATA !== undefined && env.APPDATA !== ''
+    ? `${env.APPDATA}/ASP.NET/https`
+    : `${env.HOME}/.aspnet/https`;
+
+const certificateName = "demo.client";
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  if (0 !== child_process.spawnSync('dotnet', [
+    'dev-certs',
+    'https',
+    '--export-path',
+    certFilePath,
+    '--format',
+    'Pem',
+    '--no-password',
+  ], { stdio: 'inherit' }).status) {
+    throw new Error("Could not create certificate.");
+  }
+}
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  // Hier können Sie weitere Next.js-Konfigurationen hinzufügen
+};
+
+export default nextConfig;
